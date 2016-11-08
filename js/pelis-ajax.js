80bdaf88f76fbf3f8cbdbc867d0169d86f25a575
manana <manana>
2016-11-08 13:25:49 +0100

    Version entregable de peliculas sin boton reload

--- js/pelis-ajax.js
+++ js/pelis-ajax.js
@@ -5,57 +5,60 @@ function initializeEvents(){
     $("#save").click(saveNewFilm);
     $("#modify").click(modifyFilm);
     $("#delete").click(deleteFilm);
-    $("#reload").click(initData);
     $("#fecha").datepicker();
+    //Se comenta el boton reload por no ser necesario
+    //$("#reload").click(initData);
 }
 
 
 function initData(){
+    //Al cargar la pagina recuperamos los datos del fichero json
     peticionAjaxGet();
 }
+
+function saveNewFilm(){
+    peticionAjaxPost();
+    //reloadTableBody();
+    peticionAjaxGet();
+    //Limpiar los campos del formulario
+    showInForm("","","","");
+}
+
+function modifyFilm(){
+    //Recuperar el id de la pelicula seleccionada para modificar la que estará en el formulario
+    let rowToModify = $('#table-body .film-row.row-selected .td-id').text();
+    peticionAjaxPut(rowToModify);
+    //Limpiar los campos del formulario
+    showInForm("","","","");
+}
+
+function deleteFilm(){
+    //TODO:Recuperamos los ids de las filas que están seleccionadas para un borrado multiple
+    //Recuperar el id de la pelicula seleccionada para borrar que estará en el formulario
+    let rowToDelete = $('#table-body .film-row.row-selected .td-id').text();
+    peticionAjaxDelete(rowToDelete);
+    //Limpiar los campos del formulario
+    showInForm("","","","");
+}
+
 function selectRow(){
     //Eliminamos las anteriores clases seleccionadas
     clearRowSelected();
+    //TODO:Crear una clase row selected que incluya el color de background
     $(this).addClass('row-selected');
     $(this).css("background-color", "#ff0");
+    //Mostramos en el formulario la fila seleccionada de la tabla
     showInForm($('.row-selected').find('.td-titulo').text(),$('.row-selected').find('.td-director').text(),
         $('.row-selected').find('.td-sinopsis').text(),$('.row-selected').find('.td-fecha').text());
     
 }
 
 function clearRowSelected(){
+    //TODO:Crear una clase row selected que incluya el color de background
     $('#table-body .film-row').each(function(){
         $(this).removeClass('row-selected');
         $(this).css("background-color", "#fff");
     });
-}
-
-function saveNewFilm(){
-    peticionAjaxPost();
-    reloadTableBody();
-    peticionAjaxGet();
-}
-
-function modifyFilm(){
-    let rowToModify = $('#table-body .film-row.row-selected .td-id').text();
-
-    peticionAjaxPut(rowToModify);
-}
-
-function deleteFilm(){
-    //Recuperamos los ids de las filas que están seleccionadas para un borrado multiple
- /*   let rowsToDelete=[];
-    $('#table-body .film-row .td-checked').each(function(){
-        if ($(this)[0].firstChild.checked){
-               
-            }
-        });
-        */
-    //TODO:Recuperar el id de la pelicula seleccionada para borrar que estará en el formulario
-    let rowToDelete = $('#table-body .film-row.row-selected .td-id').text();
-    peticionAjaxDelete(rowToDelete);
- //   reloadTableBody();
-//    $.when(reloadTableBody).then(peticionAjaxGet());
 }
 
 function peticionAjaxPut(rowToModify){
@@ -71,7 +74,7 @@ function peticionAjaxPut(rowToModify){
         dataType:"json",
         //URL de comunicacion con el servicio
         url: "http://localhost:3000/peliculas/"+rowToModify
-    }).done(peticionCompletadaNoReload).fail(peticionFallida);
+    }).done(peticionCompletadaStatusWait).fail(peticionFallida);
 }
 
 function peticionAjaxPost(){
@@ -115,17 +118,21 @@ function peticionAjaxDelete(rowToDelete){
         dataType:"json",
         //URL de comunicacion con el servicio
         url: "http://localhost:3000/peliculas/"+rowToDelete
-    }).done(peticionCompletadaNoReload).fail(peticionFallida);
+    }).done(peticionCompletadaStatusWait).fail(peticionFallida);
 }
 
 function peticionCompletada(data, status){
-    alert("Peticion completada get con status : " +status + ": "+data);
+    //alert("Peticion completada get con status : " +status + ": "+data);
     reloadTableBody();
     jsonToRowData(data);
 }
 
-function peticionCompletadaNoReload(data, status){
-    alert("Peticion completada delete con status : " +status + ": "+data);
+function peticionCompletadaStatusWait(data, status){
+    if (status == "success"){
+        //alert("Peticion completada delete con status : " +status + ": "+data);
+        reloadTableBody();
+        peticionAjaxGet();
+    }
 }
 
 function peticionFallida(jqXHR, status, error){
