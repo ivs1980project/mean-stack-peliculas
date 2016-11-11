55cd355d10340b36edd9d120026011ae49cae24a
manana <manana>
2016-11-11 13:48:07 +0100

    peliculas angular2

--- angular-films/videoclub/src/app/film-store/film-store.component.ts
+++ angular-films/videoclub/src/app/film-store/film-store.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Film} from '../modelo/film';
+import { Film } from '../modelo/film';
 
 @Component({
   selector: 'app-film-store',
@@ -7,32 +7,39 @@ import {Film} from '../modelo/film';
   styleUrls: ['./film-store.component.css']
 })
 export class FilmStoreComponent implements OnInit {
-  prueba="esto marcha";
+  prueba = "esto marcha";
   filmStore: Film[];
   constructor() {
-    this.filmStore=this.createNewFilmStore();
-   }
+    this.filmStore = this.createNewFilmStore();
+    console.log("create store");
+  }
 
   ngOnInit() {
   }
 
 
-  addFilm(film){
+  addFilm(film) {
     this.filmStore.push(film);
   }
-  deleteFilm(film){
+  deleteFilm(film) {
     //TODO
   }
-  searchFilm(film){
+  searchFilm(film) {
     //TODO
   }
 
-  createNewFilmStore():Film[]{
-    let film1 : Film = new Film(1,"Los Gonnies","Arnold",1990);
-    let film2 : Film = new Film(2,"Blade","Michael",2001);
-    let film3 : Film = new Film(3,"Terminator","Jonas",2003);
+  setOrder(filmStore) {
+    filmStore.sort();
+
+    console.log("click en el id");
+  }
+
+  createNewFilmStore(): Film[] {
+    let film1: Film = new Film(1, "Los Gonnies", "Arnold", 1990);
+    let film2: Film = new Film(2, "Blade", "Michael", 2001);
+    let film3: Film = new Film(3, "Terminator", "Jonas", 2003);
     let filmStore2: Film[] = new Array<Film>();
-    filmStore2.push(film1,film2,film3);
+    filmStore2.push(film1, film2, film3);
     return filmStore2
   }
 
--- angular-films/videoclub/src/app/modelo/film.ts
+++ angular-films/videoclub/src/app/modelo/film.ts
@@ -1,10 +1,28 @@
 export class Film {
 
     constructor(private id: number, private titulo: string,
-        private autor: string, private anio: number){
+        private autor: string, private anio: number) {
     }
-    toString():string{
-        return "Libro: \n\tTitulo: "+this.titulo +
-        "\n\tAutor: "+this.autor + "\n\tAnio: "+this.anio;
+    toString(): string {
+        return "Libro: \n\tTitulo: " + this.titulo +
+            "\n\tAutor: " + this.autor + "\n\tAnio: " + this.anio;
+    }
+    setTitulo(titulo: string): void {
+        this.titulo = titulo;
+    }
+    getTitulo(): string {
+        return this.titulo;
+    }
+    setAutor(autor: string): void {
+        this.autor = autor;
+    }
+    getAutor(): string {
+        return this.autor;
+    }
+    setAnio(anio: number): void {
+        this.anio = anio;
+    }
+    getAnio(): number {
+        return this.anio;
     }
 }
