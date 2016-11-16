435525d843c6a8f34fc29419492397f870b26c46
manana <manana>
2016-11-16 13:49:51 +0100

    Pelis ordenadas por campo String

--- angular-films/videoclub/src/app/film-store/film-store.component.ts
+++ angular-films/videoclub/src/app/film-store/film-store.component.ts
@@ -16,7 +16,6 @@ export class FilmStoreComponent implements OnInit {
   ngOnInit() {
   }
 
-
   addFilm(film) {
     this.filmStore.push(film);
   }
@@ -28,21 +27,20 @@ export class FilmStoreComponent implements OnInit {
   }
 
   setOrder(orden) {
-
     this.filmStore.sort(function (a, b) {
       switch (orden) {
         case "id":
           return a.getId() - b.getId();
-        //case "titulo":
-        //  return a.getTitulo() > b.getTitulo();
-        //case "director":
-        //  return a.getAutor() > b.getAutor();
+        case "titulo":
+          return a.getTitulo().localeCompare(b.getTitulo());
+        case "director":
+          return a.getAutor().localeCompare(b.getAutor());
         case "fecha":
           return a.getAnio() - b.getAnio();
       }
     });
-    console.log(this.filmStore);
     console.log("click en el " + orden);
+    console.log(this.filmStore);
   }
 
   createNewFilmStore(): Film[] {
@@ -52,21 +50,8 @@ export class FilmStoreComponent implements OnInit {
     let film4: Film = new Film(4, "300", "Jonas", 1998);
     let film5: Film = new Film(5, "PI", "Maciulis", 2015);
     let filmStore2: Film[] = new Array<Film>();
-    filmStore2.push(film1, film3, film2, film4, film5);
+    filmStore2.push(film5, film3, film2, film4, film1);
     return filmStore2
   }
 
 }
-
-//class Film{
-//  id:number;
-//  titulo:string;
-//  autor:string;
-//  anio:number;
-//  constructor(id,titulo,autor,anio){
-//    this.id=id;
- //   this.titulo=titulo;
-//    this.autor=autor;
-//    this.anio=anio;
-//  }
-//}
